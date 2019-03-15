86b0d4fa2fc7435f97a9b9c28b1f101ce211b993
JarJarBB <32435048+JarJarBB@users.noreply.github.com>
2019-03-15 10:59:21 +0100

    Ajout de la classe Algorithme

---
+++ Algorithme.java
@@ -0,0 +1,50 @@
+package graphealgo;
+
+import java.util.ArrayList;
+
+public final class Algorithme {
+
+    private Algorithme() {
+    }
+
+    public static int[][] matriceDesDistances(Graphe G) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static int[] rangDesSommets(Graphe G) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Graphe grapheReduitSelonTarjan(Graphe G) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static ArrayList<int[]> basesDuGrapheSelonTarjan(Graphe G) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static ArrayList<String[]> cheminsCritiques(Graphe G) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static double[] datesAuPlusTard(Graphe G) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static int[] cheminLePlusCourtSelonDjikstra(Graphe G, int depart, int destination) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Graphe grapheMinimalSelonKruskal(Graphe G) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Graphe codageDePruferVersGraphe(int[] codeDePrufer) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static int[] grapheVersCodageDePrufer(Graphe G) {
+        throw new UnsupportedOperationException();
+    }
+
+}
